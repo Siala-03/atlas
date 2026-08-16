fb62de88dee7513911142ee7416b0ae54c5adc8f
siala <sialasolutions@gmail.com>
2026-08-16 10:57:27 +0200

    Update contact email to atlassuppliesltd@gmail.com

--- src/lib/contact.ts
+++ src/lib/contact.ts
@@ -1,4 +1,4 @@
 export const CONTACT_ADDRESS = "KK 15 Rd, Kigali, Rwanda";
 export const CONTACT_PHONE_DISPLAY = "0783 677 712";
 export const CONTACT_PHONE_INTL = "+250783677712";
-export const CONTACT_EMAIL = "orders@atlassupplies.rw";
+export const CONTACT_EMAIL = "atlassuppliesltd@gmail.com";
